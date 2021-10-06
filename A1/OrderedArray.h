8451dabc61ad2e8aac3dc68db3bcc4254a3c8cde
Asper Sarras <Cristobalsarras@gmail.com>
2021-10-06 19:55:18 -0300

    A1 Finished

--- A1/OrderedArray.h
+++ A1/OrderedArray.h
@@ -1,186 +1,85 @@
 #pragma once
 #include <cassert>
+#include "Array.h"
 using namespace std;
 
 template<class T>
-class OrderedArray
+class OrderedArray : public Array<T>
 {
 private:
-	//Var
-	T* m_array; // pointer to the beginning of the array
-
-	int m_maxSize;
-	int m_growSize;
-	int m_numElements;
+	bool multBool;
 public:
-	//Constructor
-	OrderedArray(int size, int growBy = 1) :
-		m_array(NULL), m_maxSize(0), m_growSize(0), m_numElements(0)
-	{
-		if (size)
-		{
-			m_maxSize = size;
-			m_array = new T[m_maxSize]; //Dyanmic allocation
-			memset(m_array, 0, sizeof(T) * m_maxSize); //sets a default value to each value of the array
-				// memset(block of memory to fill, value to set, how much of the memory we want to set)
-			m_growSize = ((growBy > 0) ? growBy : 0); // if GrowBy>0 m_growSize=growBy, else m_growSize=0
-		}
-	}
-	//Destructor
-	~OrderedArray()
-	{
-		if (m_array != nullptr)
-		{
-			delete[] m_array;
-			m_array = nullptr;
-		}
-	}
+	OrderedArray(int size, bool multiplies) : Array<T>(size) { multBool = multiplies; }
+	OrderedArray(OrderedArray& obj) : Array<T>(obj) {};
 
 	//Insertion -- BigO =O(N)
-	void push(T val)
+	void push(T val) override
 	{
-		assert(m_array != nullptr);
-		if (m_numElements >= m_maxSize)
-		{
-			Expand();
-		}
+		assert(Array<T>::m_array != nullptr);
 
-		int i, k;
-		//Find Indext to insert
-		for (i = 0; i < m_numElements; i++)
+		int dup = 0;
+
+		if (multBool == 1)
 		{
-			if (m_array[i] > val)
+			for (int d = 0; d < Array<T>::getSize(); d++)
 			{
-				break;
-			}
-		}
-		//Shift everythig after the selected index to the right
-		for (k = m_numElements; k > i; k--)
-		{
-			m_array[k] = m_array[k - 1];
-		}
-		//Insert val into the index
-		m_array[i] = val;
-		m_numElements++;
-		//return i;
-	}
-
-	//Deletetion
-	void pop()
-	{
-		if (m_numElements > 0)
-		{
-			m_numElements--; // Reduce the last place checker by 1, ignoring the values that are past it
-		}
-	}
-	//Remove an item given an index BigO=O(N)
-	void remove(int index)
-	{
-		assert(m_array != nullptr);
-
-		if (index >= m_numElements)
-		{
-			return; //May do an exception handling
-		}
-
-		// Copy the value from i+1 into i and then i++
-		for (int i = index; i < m_numElements; i++)
-		{
-			if (i + 1 < m_numElements)
-			{
-				m_array[i] = m_array[i + 1];
-			}
-		}
-		m_numElements--;
-	}
-
-	//Searching Binary search
-	int search(T searchKey)
-	{
-		assert(m_array != nullptr);
-
-		int lowerBound = 0;
-		int upperBound = m_numElements - 1;
-		int current = 0;
-
-		//TO DO Binary Search
-		while (1)
-		{
-			current = (lowerBound + upperBound) >> 1;
-
-			//Binary Search
-			//1 Check if the middle is the value we are looking for
-			if (m_array[current] == searchKey)
-			{
-				return current;
-			}
-			//2 Check we cant find the item
-			else if (lowerBound > upperBound)
-			{
-				return -1;
-			}
-			//3 check wich half of the array is the value
-			else
-			{
-				if (m_array[current] < searchKey)
+				if (Array<T>::m_array[d] == val)
 				{
-					lowerBound = current + 1;
-				}
-				else
-				{
-					upperBound = current - 1;
+					dup++;
+					cout << "No duplicated values accepted multiples " <<val<< " will be omited." << endl;
 				}
 			}
 		}
-		return -1;
+
+		if (dup == 0)
+		{
+			if (Array<T>::getSize() >= Array<T>::getMaxSize())
+			{
+				Expand();
+			}
+
+			int i, k;
+			//Find Indext to insert
+			for (i = 0; i < Array<T>::getSize(); i++)
+			{
+				if (Array<T>::m_array[i] > val)
+				{
+					break;
+				}
+			}
+			//Shift everythig after the selected index to the right
+			for (k = Array<T>::getSize(); k > i; k--)
+			{
+				Array<T>::m_array[k] = Array<T>::m_array[k - 1];
+			}
+			//Insert val into the index
+			Array<T>::m_array[i] = val;
+			Array<T>::setSize(Array<T>::getSize() + 1);
+			//return i;
+		}
 	}
 
-	//Overloaded [] Op
-	T& operator[](int index)
-	{
-		assert(m_array != nullptr && index < m_numElements);
-		return m_array[index];
-	}
-
-	//Clear
-	void clear()
-	{
-		m_numElements = 0; //Changes the counter to the first value, ignoring the whole arrray
-	}
-
-	//Getters
-	int getSize() { return m_numElements; }
-	int getMaxSize() { return m_maxSize; }
-	int getGrowSize() { return m_growSize; }
-
-	//Setters
-	int setGrowSize(int val)
-	{
-		assert(val >= 0);
-		m_growSize = val;
-	}
-
-	//Private Functions
 private:
 	//Expantion
 	bool Expand()
 	{
-		if (m_growSize <= 0)
+		if (Array<T>::getGrowSize() <= 0)
 		{
 			return false;
 		}
 
 		//Create a new Array
-		T* temp = new T[m_maxSize + m_growSize];
+		T* temp = new T[Array<T>::getMaxSize() + (Array<T>::getGrowSize() * 2)];
 		assert(temp != nullptr);
 		//Copy content from the original array to the new one
-		memcpy(temp, m_array, sizeof(T) * m_maxSize);
+		memcpy(temp, Array<T>::m_array, sizeof(T) * Array<T>::getMaxSize());
 		//Delete old array
-		delete[] m_array;
+		delete[] Array<T>::m_array;
 		//Clean up variable assignments
-		m_array = temp;
+		Array<T>::m_array = temp;
 		temp = nullptr;
-		m_maxSize += m_growSize;
+		Array<T>::setMaxSize(Array<T>::getMaxSize() + (Array<T>::getGrowSize() * 2));
+		Array<T>::setGrowSize(Array<T>::getGrowSize() * 2);
 
 		return true;
 	}
--- A1/Source.cpp
+++ A1/Source.cpp
@@ -6,55 +6,26 @@ using namespace std;
 
 int main()
 {
+	int multiples = 3;
+
+	cout << "Would you accept multiples? 0=yes. 1=no" << endl;
+	cin >> multiples;
+
+	if (multiples > 1 || multiples < 0)
+	{
+		cout << "Incorrect answer, program will accept multiples" << endl;
+		multiples = 0;
+	}
+
+	cout << "Ordered Example" << endl;
 	//Array<int>a(5);
-	UnorderedArray<int>ar(5);
-	//UnorderedArray<int>ar(5);
+	OrderedArray<int>ar(5,multiples);
 	ar.push(2);
 	ar.push(32);
+	ar.push(11);
 	ar.push(23);
 	ar.push(89);
 	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-	ar.push(11);
-	cout << ar.getMaxSize() << endl;
-
-	cout << "Bubble sort" << endl;
-
-	cout << "Before Sorting:";
 
 	for (int i = 0; i < ar.getSize(); i++)
 	{
@@ -62,12 +33,29 @@ int main()
 	}
 	cout << endl;
 
-	ar.mergeSort();
+	cout << "Merge Sort Example" << endl;
+	
+	UnorderedArray<int>arr(5);
+	arr.push(2);
+	arr.push(32);
+	arr.push(11);
+	arr.push(23);
+	arr.push(89);
+	arr.push(11);
+
+	cout << "Before Sorting:";
+	for (int i = 0; i < arr.getSize(); i++)
+	{
+		cout << " " << arr[i];
+	}
+	cout << endl;
+
+	arr.mergeSort();
 
 	cout << "After Sorting:";
-	for (int i = 0; i < ar.getSize(); i++)
+	for (int i = 0; i < arr.getSize(); i++)
 	{
-		cout << " " << ar[i];
+		cout << " " << arr[i];
 	}
 	cout << endl;
 	
